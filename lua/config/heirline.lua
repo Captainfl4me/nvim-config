d2c84cef3df727662cb995590306cd847289d49a
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2024-01-31 23:54:33 +0000

    add git to status bar

--- lua/config/heirline.lua
+++ lua/config/heirline.lua
@@ -213,6 +213,59 @@ local FileFormat = {
     end
 }
 
+local Git = {
+    condition = conditions.is_git_repo,
+
+    init = function(self)
+        self.status_dict = vim.b.gitsigns_status_dict
+        self.has_changes = self.status_dict.added ~= 0 or self.status_dict.removed ~= 0 or self.status_dict.changed ~= 0
+    end,
+
+    hl = { fg = "orange" },
+
+
+    {   -- git branch name
+        provider = function(self)
+            return " " .. self.status_dict.head
+        end,
+        hl = { bold = true }
+    },
+    -- You could handle delimiters, icons and counts similar to Diagnostics
+    {
+        condition = function(self)
+            return self.has_changes
+        end,
+        provider = "("
+    },
+    {
+        provider = function(self)
+            local count = self.status_dict.added or 0
+            return count > 0 and ("+" .. count)
+        end,
+        hl = { fg = "git_add" },
+    },
+    {
+        provider = function(self)
+            local count = self.status_dict.removed or 0
+            return count > 0 and ("-" .. count)
+        end,
+        hl = { fg = "git_del" },
+    },
+    {
+        provider = function(self)
+            local count = self.status_dict.changed or 0
+            return count > 0 and ("~" .. count)
+        end,
+        hl = { fg = "git_change" },
+    },
+    {
+        condition = function(self)
+            return self.has_changes
+        end,
+        provider = ")",
+    },
+}
+
 
 -- Cursor position: Ruler and ScrollBar
 -- We're getting minimalists here!
@@ -229,7 +282,7 @@ local Align = { provider = "%=" }
 local Space = { provider = " " }
 
 local DefaultStatusline = {
-    ViMode, Space, FileNameBlock, Align,
+    ViMode, Space, FileNameBlock, Space, Git, Align,
 	FileType, Space, Ruler
 }
 local InactiveStatusline = {
