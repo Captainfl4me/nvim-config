d5fa9fdabb162f914fc56c06c966a25f729c88b7
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2024-02-08 10:50:02 +0000

    add comments plugin

---
+++ lua/plugins/comments.lua
@@ -0,0 +1,10 @@
+local M = {
+    "numToStr/Comment.nvim",
+    keys = {
+      { "gc", mode = { "n", "v" }, desc = "Comment toggle linewise" },
+      { "gb", mode = { "n", "v" }, desc = "Comment toggle blockwise" },
+    },
+    opts = {},
+}
+
+return M
