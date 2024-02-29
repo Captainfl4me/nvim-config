e8598b6691c6e53c1b79edd845bb278e141bf110
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2024-02-29 10:47:59 +0000

    add: autopairs

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -20,6 +20,7 @@ local M = {
 				},
 				opts = {},
 			},
+			"windwp/nvim-autopairs",
 
 		},
 		event = "InsertEnter",
@@ -30,6 +31,11 @@ local M = {
 
 			-- nvim-cmp setup
 			local cmp = require 'cmp'
+			local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+			cmp.event:on(
+			  'confirm_done',
+			  cmp_autopairs.on_confirm_done()
+			)
 			return {
 				snippet = {
 					expand = function(args)
