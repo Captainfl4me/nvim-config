6265ce1ed01391cc7e43e7d9db2c8b514267d60c
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2024-03-04 00:03:43 +0000

    Add noice support

--- init.lua
+++ init.lua
@@ -21,19 +21,19 @@ vim.opt.cursorlineopt = "number"
 -- Lazy config (package manager)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Get the directory of the current script file 
-local script_dir = debug.getinfo(1, 'S').source:match[[^@?(.*[\/])[^\/]-$]]
+-- Get the directory of the current script file
+local script_dir = debug.getinfo(1, 'S').source:match [[^@?(.*[\/])[^\/]-$]]
 -- Add the directory to the package.path
 package.path = package.path .. ';' .. script_dir .. '?.lua'
 
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,8 +4,8 @@ vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Next diagnostic" })
 
 -- replace currently selected text with default register without yanking it
-vim.keymap.set('v', '<leader>p', '"_dP')
+vim.keymap.set('v', '<leader>p', '"_dP', { desc = "Replace without yank" })
